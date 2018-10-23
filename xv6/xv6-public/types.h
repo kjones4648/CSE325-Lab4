da144e5e77096240104c471e2b70077a54320289
eegarcia00 <33458580+eegarcia00@users.noreply.github.com>
2018-10-23 00:51:24 -0600

    Lab-4 code added

--- xv6/xv6-public/types.h
+++ xv6/xv6-public/types.h
@@ -1,4 +1,18 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
 typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+/* Lab-4 Keaton Jones & Erik Garcia */
+typedef struct __lock_t {
+  uint is_locked;
+} lock_t;
+
+#ifndef NULL
+//#define NULL (0)
+#endif
+
+#endif
